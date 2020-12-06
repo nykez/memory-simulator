bdfc66be4586a76021c2acb730b5ca38f8eba793
Harrison Pollitte <pollitteh@etsu.edu>
2020-12-05 19:07:01 -0500

    Added PageFaultHandler::LRUReplacePage()

--- PageFaultHandler.h
+++ PageFaultHandler.h
@@ -24,8 +24,6 @@ namespace PageFaultHandler {
         int PFN = FindFreeFrame(PT);    // Find free frame to use.
         if(PFN == -1) {                 // Is there a free frame?
             PFN = LRUReplacePage(PT);   // If not, swap out a frame.
-            //[TODO]: Update TLB
-            //[TODO]: Update DC
         }
         // Update entry's PFN
         std::vector<int> retPFN = BinaryConverter::ToBitArray(PFN);
@@ -44,8 +42,39 @@ namespace PageFaultHandler {
        return -1;
     }
 
+    /// <summary>
+    /// Runs LRU replacement for pages.
+    /// </summary>
+    /// <param name="PT">pointer to page table</param>
+    /// <returns>PFN of victim entry.</returns>
     int LRUReplacePage(PageTable* PT) {
+        int LRU = PT->GetAccessOrdinal();
+        std::vector<int> victimPFN;
+        int victimVPN = 0;
+        // Find Least-Recently-Used entry in PT.
+        for(int i = 0; i < PT->entries.size(); i++) {
+            // If invalid, skip. (It doesn't have a frame).
+            if(PT->GetEntryValidity(i) == false) continue;
+            // If least recent so far...
+            if(PT->entries.at(i).lastUsed < LRU) {
+                LRU = PT->entries.at(i).lastUsed;   // update comparer
+                victimPFN = PT->GetEntryPFN(i);     // and update PFN
+                victimVPN = i;
+            }
+        }        
+        // Update victim entry
+        PT->SetEntryValidity(victimVPN, false);
+        // Inform TLB and DC of newly invalidated entry
+        InformTLB(victimVPN);
+        InformDC(victimVPN);
+    }
 
+    void InformTLB(int VPN) {
+        //[TODO]:
+    }
+
+    void InformDC(int VPN) {
+        //[TODO]:
     }
 } // namespace PageFaultHandler
 
