62ed219eccda1fa28fd2388fa118baf397a04aad
Harrison Pollitte <pollitteh@etsu.edu>
2020-12-06 14:12:33 -0500

    Created SimulationDeployer.h

---
+++ SimulationDeployer.h
@@ -0,0 +1,32 @@
+/// NAME: SimulationDeployer.h
+/// PURPOSE: Main controller of the program. Initial class.
+/// AUTHOR: Harrison Lee Pollitte
+/// CREATED: 12/6/2020
+/// NOTES:
+///
+///
+///
+
+#ifndef SIM_DEP_H
+#define SIM_DEP_H
+
+class SimulationDeployer
+{
+private:
+    /* data */
+public:
+    SimulationDeployer(/* args */);
+    ~SimulationDeployer();
+};
+
+SimulationDeployer::SimulationDeployer(/* args */)
+{
+}
+
+SimulationDeployer::~SimulationDeployer()
+{
+}
+
+
+
+#endif //SIM_DEP_H
