ef3e46e69bc3ed388372f066cf40c7dd809ba455
David <27027506+CodeCat08@users.noreply.github.com>
2020-12-08 15:44:46 -0500

    Updated DTLB to be more similar to PT, also updated DTLB to use TableEntry.h structs

--- LookupBuffer/DTLB.h
+++ LookupBuffer/DTLB.h
@@ -1,23 +1,38 @@
 #ifndef TLB_H
 #define TLB_H
+#include <iostream>
 #include <map>
+#include <string>
 
-#include "DTLBEntry.h"
-#include "../BinaryConverter.h"
+#include "../TableEntry.h"
 
 class TLB
 {
 	//TLB statistics
+	int tlb_references_ = 0; //amount of times DTLB has been referenced
 	int tlb_hits_ = 0; //TLB hit counter
 	int tlb_misses_ = 0; //TLB miss counter
 
-	std::map<int, DTLBEntry> mappings; //map to hold TLB mappings <VPN:TableEntry>
+	//TLB properties
+	int accessOrdinal = 1; //largest == most recently used
+	int maxSize = 0; //maximum size of DTLB
+	int size = 0; //current size of TLB
+	
+	std::map<int, TableEntry> mappings; //map to hold TLB mappings <VPN:TableEntry>
 	
 public:
 
 	TLB();
 
 	std::pair<bool, int> LookUp(int VPN);
+	void InsertEntry(TableEntry entry);
+	
+	int GetAccessOrdinal();
+	bool GetEntryValidity(int entry);
+	void SetEntryValidity(int entry, bool state);
+	int GetMaxSize();
+	void SetMaxSize(int maxSize);
+	int GetSize();
 };
 
 TLB::TLB()
@@ -33,25 +48,94 @@ TLB::TLB()
 /// of the entry.</returns>
 std::pair<bool, int> TLB::LookUp(int VPN)
 {
+	this->tlb_references_++; //update TLB references counter
+	
 	auto valid = false; //initialize the found entry's valid bit to false
 	auto pfn = -1; //initialize the found entry's PFN to -1
-
-	for (auto mapping : mappings)
+	
+	//If the TLB contains the queried VPN
+	if (mappings.contains(VPN))
 	{
-		valid = mapping.second.validBit; //is the entry valid?
-
-		//If the queried VPN is found and is valid
-		if (VPN == mapping.first && valid == true)
+		//Check if the entry's valid bit is set
+		if (mappings.at(VPN).validBit == true)
 		{
-			//Return the corresponding PFN
-			pfn = mapping.second.PFN;
-
-			this->tlb_hits_++; //increment the number of TLB hits
-			return std::pair<bool, int>{valid, pfn}; //return valid PFN
+			valid = mappings.at(VPN).validBit;
+			pfn = mappings.at(VPN).PFN;
+			
+			//if entry is in TLB and is valid
+			this->tlb_hits_++; //count TLB hit
+			
+			//update entry lru
+			mappings.at(VPN).lastUsed = accessOrdinal;
+			accessOrdinal++;
+			
+			return {valid, pfn}; //return pair indicating entry is valid
+		}
+		else
+		{
+			//If the entry is invalid
+			this->tlb_misses_++; //count TLB miss
+			return {valid, pfn}; //return pair indicating entry is invalid
 		}
 	}
+	else
+	{
+		//If the pfn couldn't be found
+		this->tlb_misses_++; //count TLB miss
+		return {valid, pfn}; //return pair indicating pfn not found
+	}
+}
 
-	//if the pfn couldn't be found
-	return std::pair<bool, int>{valid, pfn}; //return pair indicating pfn not found
+inline void TLB::InsertEntry(TableEntry entry)
+{
+	//Check if the TLB has free space
+	if (mappings.size() < maxSize)
+	{
+		//If there is free space, add entry to TLB
+		mappings.insert({entry.validBit, entry});
+	}
+	else
+	{
+		//If there isn't free space, evict entry
+		
+	}
+}
+
+int TLB::GetAccessOrdinal()
+{
+	return this->accessOrdinal;
+}
+
+bool TLB::GetEntryValidity(int VPN)
+{
+	if (mappings.contains(VPN) == true)
+	{
+		return mappings.at(VPN).validBit;
+	}
+	else
+	{
+		std::cerr << "Entry '"s + to_string(VPN) + "' not in table"s << std::endl;
+		return false;
+	}
+}
+
+void TLB::SetEntryValidity(int VPN, bool state)
+{
+	
+}
+
+int TLB::GetMaxSize()
+{
+	return this->maxSize;
+}
+
+void TLB::SetMaxSize(int maxSize)
+{
+	this->maxSize = maxSize;
+}
+
+int TLB::GetSize()
+{
+	return this->size;
 }
 #endif // TLB_H
